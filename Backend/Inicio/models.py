8506d688218a1985ec1e6b057b412dfc2bd22d14
jhonatan grajales <grajajhon9@gmail.com>
2023-12-21 00:14:01 -0500

    Para instalar django Restframwork ejecutamos el siguiente comando: pip install djangorestframework y en el archivo de Settings en la lista de aplicaciones debemos instalar rest_framework, despues ejecutamos el comando pip install django-cors-headers, que nos permitira integrar dos servidores, tambien se debe instalar corsheaders en el archivo Settings y adicional en los MIDDLEWARE, se debe agregar una linea superior a la linea de Common:corsheaders.middleware.CorsMiddleware. Se creo en el archivo models dos tablas usuario y rol, se debe ejecutar el comando python manage.puy makemgrations, para que Django lea los cambios y despues el comandopython manage.py migrate para crear las tablas

--- Backend/Inicio/models.py
+++ Backend/Inicio/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Colaborador(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Primer_Apellido = models.CharField(max_length=50)
+    Segundo_Apellido = models.CharField(max_length=50)
+    Celular = models.IntegerField()
+    Correo = models.CharField(max_length=45)
+    Cargo = models.CharField(max_length=50)
+    Historial_Laboral = models.CharField(max_length=50)
+    Estado = models.BooleanField()
+    contraseña = models.CharField(max_length=12)
+    
+    rol = models.OneToOneField(
+        'Rol',
+        on_delete = models.CASCADE,
+        primary_key = True,
+    )
+
+class Rol(models.Model):
+    Nombre = models.CharField(max_length = 50)
--- Backend/Revolution/settings.py
+++ Backend/Revolution/settings.py
@@ -37,13 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'corsheaders',#Permite la comunicación conotro Backend
+    'rest_framework',#Nos permite Crear Apis de manera más rapida
     'Inicio.apps.InicioConfig'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",#Permite la comunicación conotro Backend
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = []#Se ingresa los servidores que se pueden conectar a Django
